bdb950b22eba61d883c015cfba9e27a27edcacfc
luulu520 <a1247054169@gmail.com>
2021-10-02 19:15:14 +0800

    005

---
+++ alter.js
@@ -0,0 +1,19 @@
+function transform() {
+    let text = document.getElementById("orig").value;
+    let text_array = text.split('\n');
+    
+    let new_text = "";
+    
+    
+    text_array.forEach(line => {
+        let getran = Math.floor( Math.random() * text_array.length );
+        let newtext =text_array[getran];
+        
+        new_text += `${newtext}<br />`;
+
+    });
+
+    document.getElementById("result").innerHTML = new_text;
+}
+
+
