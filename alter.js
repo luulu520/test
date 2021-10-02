9de90d345f5b3379b9f2b5d132d32468cd6574be
luulu520 <a1247054169@gmail.com>
2021-10-02 19:17:40 +0800

    Update alter.js

--- alter.js
+++ alter.js
@@ -3,16 +3,9 @@ function transform() {
     let text_array = text.split('\n');
     
     let new_text = "";
-    
-    
-    text_array.forEach(line => {
         let getran = Math.floor( Math.random() * text_array.length );
         let newtext =text_array[getran];
-        
         new_text += `${newtext}<br />`;
-
-    });
-
     document.getElementById("result").innerHTML = new_text;
 }
 
